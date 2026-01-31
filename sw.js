5496a2944dafb6e852e40696b9355631c7089dc4
Nahuelsand <rod.arena7@gmail.com>
2026-01-30 22:30:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,50 @@
-const CACHE_NAME = 'sandeco-ball-v1';
+const CACHE_NAME = 'sandeco-ball-v2';
 const assets = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/sandeco.png'
+  '/sandeco.png',
+  // Asegúrate de que tu build genera estos archivos
+  '/assets/index-*.js',
+  '/assets/index-*.css'
 ];
 
 self.addEventListener('install', (evt) => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
+      console.log('Cacheando assets');
+      return cache.addAll(assets).catch(err => {
+        console.log('Error cacheando algunos assets:', err);
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
     })
   );
 });
 
 self.addEventListener('fetch', (evt) => {
   evt.respondWith(
-    fetch(evt.request).catch(() => {
-      return caches.match(evt.request);
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    }).catch(() => {
+      // Si falla todo, devolver offline page si existe
+      if (evt.request.mode === 'navigate') {
+        return caches.match('/index.html');
+      }
     })
   );
 });
-
