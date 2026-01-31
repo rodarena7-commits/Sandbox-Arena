d7fddbe40dfa8264ae3ad554484aee336221a755
Nahuelsand <rod.arena7@gmail.com>
2026-01-31 17:29:00 -0300

    Create version.js

---
+++ version.js
@@ -0,0 +1,5 @@
+// version.js - Mantener versión sincronizada entre SW y App
+export const APP_VERSION = '1.1.0';
+export const CACHE_VERSION = 'sandeco-ball-v1.1.0';
+export const STATIC_CACHE = 'sandeco-static-v1.1.0';
+export const DYNAMIC_CACHE = 'sandeco-dynamic-v1.1.0';
